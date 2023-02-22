f0a85dd23b4dace3e3487db496c34e3cf979d764
anqeloh <122122517+anqeloh@users.noreply.github.com>
2023-02-21 18:34:57 -0800

    Challenge Done

--- Prototype-Three/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Prototype-Three/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public bool gameOver;
+    public bool isLowEnough;
+
+    public AudioClip BounceSound;
 
     public float floatForce;
     private float gravityModifier = 1.5f;
@@ -21,6 +24,8 @@ public class PlayerControllerX : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        playerRb = GetComponent<Rigidbody>();
+
         Physics.gravity *= gravityModifier;
         playerAudio = GetComponent<AudioSource>();
 
@@ -33,9 +38,18 @@ public class PlayerControllerX : MonoBehaviour
     void Update()
     {
         // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        if (Input.GetKey(KeyCode.Space)&& isLowEnough && !gameOver)
         {
             playerRb.AddForce(Vector3.up * floatForce);
+        }
+
+        if (transform.position.y > 13)
+        {
+            isLowEnough = false;
+        }
+        else
+        {
+            isLowEnough = true;
         }
     }
 
@@ -58,6 +72,11 @@ public class PlayerControllerX : MonoBehaviour
             playerAudio.PlayOneShot(moneySound, 1.0f);
             Destroy(other.gameObject);
 
+        }
+        else if (other.gameObject.CompareTag("Ground") && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            playerAudio.PlayOneShot(BounceSound, 1.5f);
         }
 
     }
